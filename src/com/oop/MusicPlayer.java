52b498ee77b1b6844d3a5357d7f77e609f0a16b0
Dianique <81393644+Dianique@users.noreply.github.com>
2021-06-14 20:10:59 -0400

    Update MusicPlayer.java

--- src/com/oop/MusicPlayer.java
+++ src/com/oop/MusicPlayer.java
@@ -1,11 +1,10 @@
 package com.oop;
 
 
-import java.sql.SQLOutput;
+public class MusicPlayer { // This is my main class called Music Player.
 
-public class MusicPlayer {
+    public static void main(String[] args) { //I created new instantiated objects as vocalist, guitarist, and trumpet player with their specific fields included in parentheses. I then called the show method on each to display in the terminal.
 
-    public static void main(String[] args) {
        Musician esperanza = new Vocalist ("ESPERANZA SPALDING", "USA","JAZZ",2000, "RON CARTER"); //
        esperanza.show();
        Musician prince = new Guitarist ("PRINCE", "USA","ROCK",1980, "SIX");
@@ -14,20 +13,27 @@ public class MusicPlayer {
        satchmo.show();
        System.out.println(".......................");
 
+
         System.out.println("List of Songs");
         Musician burnaB = new Musician ("Burna Boy", "NIGERIA", "AFRO BEATS", 2000);
         Musician alicia = new Musician ("Alicia Keys", "USA", "R&B", 2000);
-        Musician donnyH = new Musician ("Donny Hathaway", "USA", "SOUL", 1950);
+        Musician donnyH = new  Musician ("Donny Hathaway", "USA", "SOUL", 1950);
         Musician grupoN= new Musician ("Grupo Niche", "COLOMBIA", "SALSA", 1990);
 
-        Playlist deeTunes = new Playlist(); // Creates a new instantiated object Playlist. (Follow-up on this method for greater understanding)
 
-// I'm calling the Playlist deeTunes add method to add each song into my deeTunes Arraylist. It has two functions to add a song and instantiate the object.
-       deeTunes.add(new Song("Ye",  burnaB , "Vibe", "https://www.youtube.com/watch?v=4rz_kQiBxaQ"));
-       deeTunes.add(new Song("Lovin U",  alicia, "R&B", "https://www.youtube.com/watch?v=icQ6W97f1gI"));
-       deeTunes.add(new Song("A Song For You", donnyH, "Soul", "https://www.youtube.com/watch?v=VE1nh7KREcA"));
-       deeTunes.add(new Song("Culebra",  grupoN, "Salsa", "https://www.youtube.com/watch?v=Y3Y9YKncNRo"));
-       deeTunes.displaySongs();
+        Playlist myTunes = new Playlist();// Creates a new instantiated object Playlist.
+// I'm calling the Playlist myTunes (add method) to add each song into my myTunes Arraylist. It has two functions to add a song and instantiate the object.
+       myTunes.add(new Song("Ye", burnaB, "Vibe", "https://www.youtube.com/watch?v=lPe09eE6Xio"));
+       // OR... TO BE WRITTEN AS: Song newBurna = new Song ("Ye", burnaB, "Vibe", "https://www.youtube.com/watch?v=lPe09eE6Xio");
+      //                          newBurna.show();
+        myTunes.add(new Song("Lovin U",  alicia, "R&B", "https://www.youtube.com/watch?v=icQ6W97f1gI"));
+        myTunes.add(new Song("A Song For You", donnyH, "Soul", "https://www.youtube.com/watch?v=VE1nh7KREcA"));
+        myTunes.add(new Song("Culebra",  grupoN, "Salsa", "https://www.youtube.com/watch?v=Y3Y9YKncNRo"));
+        myTunes.displaySongs();
     }
         }
+
+
+
+
 
